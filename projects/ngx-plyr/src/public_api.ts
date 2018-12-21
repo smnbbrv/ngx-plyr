8eaee28ef54ec9e06f48964682920575a6eb3e51
smnbbrv <simonellez@gmail.com>
2018-12-21 15:36:31 +0100

    refactor: rename plyr.module

--- projects/ngx-plyr/src/public_api.ts
+++ projects/ngx-plyr/src/public_api.ts
@@ -1,4 +1,4 @@
 export * from './lib/plyr-driver/plyr-driver';
 export * from './lib/plyr-driver/default-plyr-driver';
 export * from './lib/plyr/plyr.component';
-export * from './lib/ngx-plyr.module';
+export * from './lib/plyr.module';
